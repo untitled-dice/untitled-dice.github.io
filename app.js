cb4d18143b1e1b33ffd25d916705225777716ba7
untitled-dice <callicles@fea.st>
2015-05-16 21:03:50 -0500

    Remind user to edit recaptcha_sitekey

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <------------------ EDIT ME!
+  app_id: 18,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'Untitled Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',
+  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
   redirect_uri: 'https://untitled-dice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',

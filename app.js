ffa7426fdd04881fdba8a2be338bed0d052486b5
untitled-dice <callicles@fea.st>
2015-08-13 20:36:27 -0500

    Implement configurable house edge

--- app.js
+++ app.js
@@ -18,11 +18,35 @@ var config = {
   debug: isRunningLocally(),
   // - Set this to true if you want users that come to http:// to be redirected
   //   to https://
-  force_https_redirect: !isRunningLocally()
+  force_https_redirect: !isRunningLocally(),
+  // - Configure the house edge (default is 1%)
+  //   Must be between 0.0 (0%) and 1.0 (100%)
+  house_edge: 0.01
 };
 
 ////////////////////////////////////////////////////////////
 // You shouldn't have to edit anything below this line
+////////////////////////////////////////////////////////////
+
+// Validate the configured house edge
+(function() {
+  var errString;
+
+  if (config.house_edge <= 0.0) {
+    errString = 'House edge must be > 0.0 (0%)';
+  } else if (config.house_edge >= 100.0) {
+    errString = 'House edge must be < 1.0 (100%)';
+  }
+
+  if (errString) {
+    alert(errString);
+    throw new Error(errString);
+  }
+
+  // Sanity check: Print house edge
+  console.log('House Edge:', (config.house_edge * 100).toString() + '%');
+})();
+
 ////////////////////////////////////////////////////////////
 
 if (config.force_https_redirect && window.location.protocol !== "https:") {
@@ -64,7 +88,10 @@ helpers.multiplierToWinProb = function(multiplier) {
   console.assert(typeof multiplier === 'number');
   console.assert(multiplier > 0);
 
-  return 0.99 / multiplier;
+  // For example, n is 0.99 when house edge is 1%
+  var n = 1.0 - config.house_edge;
+
+  return n / multiplier;
 };
 
 helpers.calcNumber = function(cond, winProb) {
